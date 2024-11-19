ea0e227ad05c7b6d8deb4b3be13cc7b882c53d2a
Karim Elmestari <81093448+Karrimmel@users.noreply.github.com>
2024-11-19 14:29:01 +0100

    AppBar and Student Dashboard setup

---
+++ frontend/src/pages/StudentDashboard.js
@@ -0,0 +1,12 @@
+import  PrimarySearchAppBar  from '../components/AppBar'
+
+
+function StudentDashboard() {
+    return (
+      <div className="App">
+            <PrimarySearchAppBar/>
+      </div>
+    );
+  }
+  
+  export default StudentDashboard;
