feb98cd9aab062d8e7eadb108f3671c401947165
DamienBataillard <damien.bataillard@efrei.net>
2024-12-03 14:52:44 +0100

    css fixe

--- frontend/src/pages/StudentDashboard.js
+++ frontend/src/pages/StudentDashboard.js
@@ -7,16 +7,16 @@ import '../index.css';
 function StudentDashboard() {
   const navigate = useNavigate();
   const [evaluations, setEvaluations] = useState([]);
-  const [modules, setModules] = useState([]); // State pour les modules
+  const [modules, setModules] = useState([]);
   const [loadingEvaluations, setLoadingEvaluations] = useState(true);
-  const [loadingModules, setLoadingModules] = useState(true); // Chargement des modules
+  const [loadingModules, setLoadingModules] = useState(true);
   const [errorEvaluations, setErrorEvaluations] = useState('');
-  const [errorModules, setErrorModules] = useState(''); // Erreur pour les modules
+  const [errorModules, setErrorModules] = useState('');
 
   useEffect(() => {
     const verifyToken = async () => {
       try {
-        const token = localStorage.getItem('token'); // Récupère le token depuis localStorage
+        const token = localStorage.getItem('token');
         if (!token) {
           throw new Error('Token manquant');
         }
@@ -24,7 +24,7 @@ function StudentDashboard() {
         const response = await fetch('http://localhost:5000/api/dashboard', {
           method: 'GET',
           headers: {
-            Authorization: `Bearer ${token}`, // En-tête Authorization avec le token
+            Authorization: `Bearer ${token}`,
           },
         });
 
@@ -36,17 +36,16 @@ function StudentDashboard() {
         console.log('Données du tableau de bord:', data);
       } catch (error) {
         console.error(error.message);
-        navigate('/'); // Redirige vers la page de connexion si non autorisé
+        navigate('/'); // Redirection si non autorisé
       }
     };
 
     verifyToken();
   }, [navigate]);
 
-  // Récupérer les évaluations
   useEffect(() => {
     const fetchEvaluations = async () => {
-      const studentId = localStorage.getItem('studentId'); // ID de l'étudiant connecté
+      const studentId = localStorage.getItem('studentId');
       if (!studentId) {
         setErrorEvaluations('Aucun ID étudiant trouvé.');
         setLoadingEvaluations(false);
@@ -71,10 +70,9 @@ function StudentDashboard() {
     fetchEvaluations();
   }, []);
 
-  // Récupérer les modules
   useEffect(() => {
     const fetchModules = async () => {
-      const studentId = localStorage.getItem('studentId'); // ID de l’étudiant connecté
+      const studentId = localStorage.getItem('studentId');
       if (!studentId) {
         setErrorModules('Aucun ID étudiant trouvé.');
         setLoadingModules(false);
@@ -99,10 +97,8 @@ function StudentDashboard() {
     fetchModules();
   }, []);
 
-  if (loadingEvaluations || loadingModules)
-    return <p>Chargement...</p>;
-  if (errorEvaluations || errorModules)
-    return <p>Erreur : {errorEvaluations || errorModules}</p>;
+  if (loadingEvaluations || loadingModules) return <p>Chargement...</p>;
+  if (errorEvaluations || errorModules) return <p>Erreur : {errorEvaluations || errorModules}</p>;
 
   return (
     <div className="App">
@@ -112,98 +108,102 @@ function StudentDashboard() {
         <h2 className="subtitle">Give your thoughts</h2>
         <Box
           sx={{
-            backgroundColor: 'white',
+            backgroundColor: '#5481c2',
+            display: 'flex',
+            justifyContent: 'space-between',
             borderRadius: '10px',
             width: '80%',
             margin: 'auto',
             mt: '5%',
             mb: '5%',
-            display: 'flex',
-            flexDirection: 'column',
             boxShadow: 3,
             overflow: 'hidden',
             padding: '20px',
           }}
         >
-          <Typography variant="h6" align="center" sx={{ mb: 3 }}>
-            Available Surveys
-          </Typography>
-
-          {evaluations.length === 0 ? (
-            <Typography variant="body1" align="center">
-              No surveys available at the moment.
+          {/* Section Evaluations */}
+          <Box sx={{ flex: 1, marginRight: '20px' }}>
+            <Typography variant="h6" align="center" sx={{ mb: 3 , color: '#333'}}>
+              Available Surveys
             </Typography>
-          ) : (
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-              {evaluations.map((evaluation) => (
-                <Box
-                  key={evaluation.id_evaluation}
-                  sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    backgroundColor: '#f9f9f9',
-                    borderRadius: '5px',
-                    padding: '10px 20px',
-                    boxShadow: 2,
-                  }}
-                >
-                  <Typography variant="subtitle1">{evaluation.title_evaluation}</Typography>
-                  <Box sx={{ textAlign: 'right' }}>
-                    <Typography variant="body2">
-                      Open: {new Date(evaluation.date_opening).toLocaleDateString()}
-                    </Typography>
-                    <Typography variant="body2">
-                      Close: {new Date(evaluation.date_closing).toLocaleDateString()}
-                    </Typography>
+            {evaluations.length === 0 ? (
+              <Typography variant="body1" align="center" sx={{color: '#333'}}>
+                No surveys available at the moment.
+              </Typography>
+            ) : (
+              <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, color: '#333' }}>
+                {evaluations.map((evaluation) => (
+                  <Box
+                    key={evaluation.id_evaluation}
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      backgroundColor: '#c0cae3',
+                      borderRadius: '5px',
+                      padding: '10px 20px',
+                      boxShadow: 2,
+                    }}
+                  >
+                    <Typography variant="subtitle1" sx={{color: '#333'}}>{evaluation.title_evaluation}</Typography>
+                    <Box sx={{ textAlign: 'right', color: '#333' }}>
+                      <Typography variant="body2" sx={{color: '#333'}} >
+                        Open: {new Date(evaluation.date_opening).toLocaleDateString()}
+                      </Typography>
+                      <Typography variant="body2" sx={{color: '#333'}}>
+                        Close: {new Date(evaluation.date_closing).toLocaleDateString()}
+                      </Typography>
+                    </Box>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => navigate(`/evaluation/${evaluation.id_evaluation}`)}
+                    >
+                      Start
+                    </Button>
                   </Box>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => navigate(`/evaluation/${evaluation.id_evaluation}`)}
-                  >
-                    Start
-                  </Button>
-                </Box>
-              ))}
-            </Box>
-          )}
+                ))}
+              </Box>
+            )}
+          </Box>
 
-          <Typography variant="h6" align="center" sx={{ mt: 5, mb: 3 }}>
-            Modules
-          </Typography>
-
-          {modules.length === 0 ? (
-            <Typography variant="body1" align="center">
-              No modules available at the moment.
+          {/* Section Modules */}
+          <Box sx={{ flex: 1, marginLeft: '20px' }}>
+            <Typography variant="h6" align="center" sx={{ mb: 3, color:'#333' }}>
+              Modules
             </Typography>
-          ) : (
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-              {modules.map((module) => (
-                <Box
-                  key={module.id_module}
-                  sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    backgroundColor: '#f9f9f9',
-                    borderRadius: '5px',
-                    padding: '10px 20px',
-                    boxShadow: 2,
-                  }}
-                >
-                  <Typography variant="subtitle1">{module.name_module}</Typography>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => navigate(`/module/${module.id_module}/comments`)}
+            {modules.length === 0 ? (
+              <Typography variant="body1" align="center" sx={{color:'#333' }}>
+                No modules available at the moment.
+              </Typography>
+            ) : (
+              <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                {modules.map((module) => (
+                  <Box
+                    key={module.id_module}
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      backgroundColor: '#c0cae3',
+                      borderRadius: '5px',
+                      padding: '10px 20px',
+                      boxShadow: 2,
+                    }}
                   >
-                    View Feedbacks
-                  </Button>
-                </Box>
-              ))}
-            </Box>
-          )}
+                    <Typography variant="subtitle1" sx={{color:'#333' }}>{module.name_module}</Typography>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => navigate(`/module/${module.id_module}/comments`)}
+                    >
+                      View Feedbacks
+                    </Button>
+                  </Box>
+                ))}
+              </Box>
+            )}
+          </Box>
         </Box>
       </div>
     </div>
